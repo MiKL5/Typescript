5026edb70613c71d26873af21cf04f7dceab14e5
MiKL <miklgaillard@gmail.com>
2023-05-25 16:04:51 +0200

    Add a toggle

--- Projet/cloneTrello/scripts/app.js
+++ Projet/cloneTrello/scripts/app.js
@@ -1,9 +1,13 @@
 "use strict";
-const itemsContainer = document.querySelectorAll(('items-containers'));
+const itemsContainer = document.querySelectorAll('items-containers');
+// manipuler les items à l'intérieur des containers pour faire moins de répétition et de code
+let actualContainer, actualBtn, actualUL, actualForm, actualTextInput, actualValidation;
 function addContainerListeners(currentContainer) {
-    // supprimer un container
-    const currentContainerDeletion = currentContainer.querySelector('.delete-container-btn');
-    deleteBtnListeners(currentContainerDeletion);
+    // supprimer un container // usage de current car ça sera ajouter à tous les éléments d'un container
+    const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn');
+    const currentAddItemBtn = currentContainer.querySelector('.add-item-btn');
+    deleteBtnListeners(currentContainerDeletionBtn);
+    addItemBtnListeners(currentAddItemBtn);
 } // qui déclanche deleteBtnListeners
 itemsContainer.forEach((container) => {
     addContainerListeners(container);
@@ -11,9 +15,37 @@ itemsContainer.forEach((container) => {
 function deleteBtnListeners(btn) {
     btn.addEventListener('click', handleContainerDeletion); // qui permet de gérer handleContainerDeletion
 }
+function addItemBtnListeners(btn) {
+    btn.addEventListener('click', handleAddItem);
+}
 function handleContainerDeletion(e) {
     const btn = e.target;
     const btnsArray = [...document.querySelectorAll('.delete-container-btn')];
     const containers = [...document.querySelectorAll('.items-container')];
     containers[btnsArray.indexOf(btn)].remove();
+}
+function handleAddItem(e) {
+    const btn = e.target;
+    if (actualContainer)
+        toggleForm(actualBtn, actualForm, false); // si un bouton est ouvert, fermer l'autre
+    setContainerItems(btn); //permet d'utilier les variables dynamiques
+    toggleForm(actualBtn, actualForm, true);
+}
+function toggleForm(btn, form, action) {
+    if (!action) { // si c'est sur false, renvoi true et exécute l'action
+        form.style.display = "none";
+        btn.style.display = "block";
+    }
+    else if (action) {
+        form.style.display = "block";
+        btn.style.display = "none";
+    }
+}
+function setContainerItems(btn) {
+    actualBtn = btn;
+    actualContainer = btn.parentElement;
+    actualUL = actualContainer.querySelector('ul');
+    actualForm = actualContainer.querySelector('form');
+    actualTextInput = actualContainer.querySelector('input');
+    actualValidation = actualContainer.querySelector('.validation-msg');
 }
--- Projet/cloneTrello/src/app.ts
+++ Projet/cloneTrello/src/app.ts
@@ -1,9 +1,20 @@
-const itemsContainer = document.querySelectorAll(('items-containers')) as NodeListOf<HTMLDivElement>
+const itemsContainer = document.querySelectorAll('items-containers') as NodeListOf<HTMLDivElement>
 
-function addContainerListeners(currentContainer: HTMLDivElement) { // sert à ajouter tout les listeners
-    // supprimer un container
-    const currentContainerDeletion = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement;
-    deleteBtnListeners(currentContainerDeletion)
+// manipuler les items à l'intérieur des containers pour faire moins de répétition et de code
+let actualContainer: HTMLDivElement,
+    actualBtn: HTMLButtonElement,
+    actualUL: HTMLUListElement,
+    actualForm: HTMLFormElement,
+    actualTextInput: HTMLInputElement,
+    actualValidation: HTMLSpanElement;
+
+    function addContainerListeners(currentContainer: HTMLDivElement) { // sert à ajouter tout les listeners
+    // supprimer un container // usage de current car ça sera ajouter à tous les éléments d'un container
+    const currentContainerDeletionBtn = currentContainer.querySelector('.delete-container-btn') as HTMLButtonElement;
+    const currentAddItemBtn = currentContainer.querySelector('.add-item-btn') as HTMLButtonElement
+
+    deleteBtnListeners(currentContainerDeletionBtn)
+    addItemBtnListeners(currentAddItemBtn)
 }// qui déclanche deleteBtnListeners
 
 itemsContainer.forEach((container: HTMLDivElement) => {
@@ -13,10 +24,39 @@ itemsContainer.forEach((container: HTMLDivElement) => {
 function deleteBtnListeners(btn: HTMLButtonElement) { // qui ajoute l'eventListener
     btn.addEventListener('click', handleContainerDeletion) // qui permet de gérer handleContainerDeletion
 }
+function addItemBtnListeners(btn: HTMLButtonElement) {
+    btn.addEventListener('click', handleAddItem)
+}
 
 function handleContainerDeletion(e: MouseEvent){
     const btn = e.target as HTMLButtonElement;
     const btnsArray = [...document.querySelectorAll('.delete-container-btn')] as HTMLButtonElement[];
     const containers = [...document.querySelectorAll('.items-container')] as HTMLDivElement[];
     containers[btnsArray.indexOf(btn)].remove()
+}
+
+function handleAddItem(e: MouseEvent) {
+    const btn = e.target as HTMLButtonElement;
+    if(actualContainer) toggleForm(actualBtn, actualForm, false); // si un bouton est ouvert, fermer l'autre
+    setContainerItems(btn); //permet d'utilier les variables dynamiques
+    toggleForm(actualBtn, actualForm, true)
+}
+
+function toggleForm(btn: HTMLButtonElement, form: HTMLFormElement, action: Boolean) {
+    if(!action) { // si c'est sur false, renvoi true et exécute l'action
+        form.style.display = "none";
+        btn.style.display = "block";
+    } else if (action) {
+        form.style.display = "block";
+        btn.style.display = "none";
+    }
+}
+
+function setContainerItems(btn: HTMLButtonElement) { // gérer le formulaire
+    actualBtn = btn;
+    actualContainer = btn.parentElement as HTMLDivElement;
+    actualUL = actualContainer.querySelector('ul') as HTMLUListElement;
+    actualForm = actualContainer.querySelector('form') as HTMLFormElement;   
+    actualTextInput = actualContainer.querySelector('input') as HTMLInputElement;   
+    actualValidation = actualContainer.querySelector('.validation-msg') as HTMLSpanElement;   
 }
